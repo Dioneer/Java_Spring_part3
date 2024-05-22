21d81bf65a5680de9edecc6803f87d6b64d87155
Dioneer <gulkina_em@mail.ru>
2024-05-22 19:24:14 +0300

    add eureca gateway

--- demo/src/main/java/Pegas/ApplicationRunner.java
+++ demo/src/main/java/Pegas/ApplicationRunner.java
@@ -2,10 +2,18 @@ package Pegas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class);
+    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("controller2", r-> r.path("/api/v2/**").uri("http://localhost:8082/")).build();
     }
 }
---
+++ demo2/src/main/java/pegas/controller/Controller.java
@@ -0,0 +1,16 @@
+package pegas.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2")
+public class Controller {
+
+    @GetMapping("/Hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok().body("hello");
+    }
+}
